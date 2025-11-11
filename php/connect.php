5570fbe8a9facfe7416b3115c9dd87464b7886cd
hipsty1 <123240066@student.upnyk.ac.id>
2025-11-11 13:13:52 +0700

    perbaikkan struktur

---
+++ php/connect.php
@@ -0,0 +1,17 @@
+<?php
+// Konfigurasi koneksi
+$host     = "localhost";
+$username = "root";    
+$password = "";
+$database = "db_iLeague";
+
+// Buat koneksi
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+echo "Koneksi berhasil!";
+?>
